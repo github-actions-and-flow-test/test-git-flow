f61e55dc496580f2517b7a8ad627acd991dc7334
Edmon Narmetov <enarmetov@alfabank.ru>
2023-02-18 18:27:05 +0300

    tests and webpack

---
+++ src/component.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export const App = () => {
+
+	const [theme, setTheme] = useState<'black' | 'white'>('black')
+
+	const colorHandler = () => {
+		setTheme((prevState) => prevState === 'white' ? 'black' : 'white')
+	}
+
+	return (
+		<div style={{ background: theme, width: '100%', fontSize: 25, color: 'red', height: '100vh' }}>
+			{ theme === 'black' && 'black' }
+			<button
+				data-test-id='button'
+				onClick={colorHandler}
+			>
+				change theme lol
+			</button>
+		</div>
+	)
+}
